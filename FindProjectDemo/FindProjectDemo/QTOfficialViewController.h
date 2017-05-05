30612bdd992e351317f7afa2f177f0c670b5af7f
I <I@LeedeMacBook-Pro.local>
2017-05-05 17:53:33 +0800

    添加 官方 和 用户晒图 控制器

---
+++ FindProjectDemo/FindProjectDemo/QTOfficialViewController.h
@@ -0,0 +1,13 @@
+//
+//  QTOfficialViewController.h
+//  FindProjectDemo
+//
+//  Created by I on 2017/5/5.
+//  Copyright © 2017年 QT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTOfficialViewController : UIViewController
+
+@end
---
+++ FindProjectDemo/FindProjectDemo/QTPinterestViewController.h
@@ -0,0 +1,13 @@
+//
+//  QTPinterestViewController.h
+//  FindProjectDemo
+//
+//  Created by I on 2017/5/5.
+//  Copyright © 2017年 QT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTPinterestViewController : UIViewController
+
+@end
