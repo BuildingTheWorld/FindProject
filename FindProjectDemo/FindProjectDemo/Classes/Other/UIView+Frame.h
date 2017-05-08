150e0d17455ee528de9db260a7637b9f80126b5a
I <I@LeedeMacBook-Pro.local>
2017-05-08 12:48:12 +0800

    添加 UIView frame 分类

---
+++ FindProjectDemo/FindProjectDemo/Classes/Other/UIView+Frame.h
@@ -0,0 +1,23 @@
+//
+//  UIView+Frame.h
+//  BuDeJie
+//
+//  Created by xmg on 16/4/25.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+
+
+// 避免冲突，加前缀
+
+@interface UIView (Frame)
+
+@property CGFloat xmg_width;
+@property CGFloat xmg_height;
+@property CGFloat xmg_x;
+@property CGFloat xmg_y;
+
+@property CGFloat xmg_centerX;
+@property CGFloat xmg_centerY;
+
+@end
