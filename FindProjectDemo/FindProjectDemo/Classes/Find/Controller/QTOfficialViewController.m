dcc464c798d2c81823ab0ec9134a82bdd867f854
I <I@LeedeMacBook-Pro.local>
2017-05-09 11:58:49 +0800

    官方 用户 collectionView 完成

--- FindProjectDemo/FindProjectDemo/Classes/Find/Controller/QTOfficialViewController.m
+++ FindProjectDemo/FindProjectDemo/Classes/Find/Controller/QTOfficialViewController.m
@@ -8,17 +8,80 @@
 
 #import "QTOfficialViewController.h"
 
-@interface QTOfficialViewController ()
+@interface QTOfficialViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (strong, nonatomic) UICollectionView *officialCollectionView;
 
 @end
 
 @implementation QTOfficialViewController
 
+
+#pragma mark - 懒加载
+
+- (UICollectionView *)officialCollectionView
+{
+    if (_officialCollectionView == nil) {
+        
+        UICollectionViewFlowLayout *officialLayout = [[UICollectionViewFlowLayout alloc] init];
+        
+        officialLayout.itemSize = CGSizeMake(SCREEN_WIDTH, 333 * SCALE_6S_HEIGHT);
+        
+        officialLayout.minimumLineSpacing = 0;
+        
+        officialLayout.minimumInteritemSpacing = 0;
+        
+        _officialCollectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:officialLayout];
+        
+        _officialCollectionView.dataSource = self;
+        
+        _officialCollectionView.delegate = self;
+        
+//        collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+
+        [_officialCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"officialCollectionViewCell"];
+        
+        
+    }
+    
+    return _officialCollectionView;
+}
+
+#pragma mark - view life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:self.officialCollectionView];
 }
+
+#pragma mark - officialCollectionView datasource
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 50;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.officialCollectionView dequeueReusableCellWithReuseIdentifier:@"officialCollectionViewCell" forIndexPath:indexPath];
+    
+    cell.contentView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255) / 255.0
+                                                       green:arc4random_uniform(255) / 255.0
+                                                        blue:arc4random_uniform(255) / 255.0
+                                                       alpha:1];
+    
+    return cell;
+}
+
+
+#pragma mark - officialCollectionView delegate
+
+
 
 
 
--- FindProjectDemo/FindProjectDemo/Classes/Find/Controller/QTPinterestViewController.m
+++ FindProjectDemo/FindProjectDemo/Classes/Find/Controller/QTPinterestViewController.m
@@ -8,17 +8,72 @@
 
 #import "QTPinterestViewController.h"
 
-@interface QTPinterestViewController ()
+@interface QTPinterestViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (strong, nonatomic) UICollectionView *pinterestCollectionView;
 
 @end
 
 @implementation QTPinterestViewController
 
+#pragma mark - 懒加载
+
+- (UICollectionView *)pinterestCollectionView
+{
+    if (_pinterestCollectionView == nil)
+    {
+        UICollectionViewFlowLayout *pinteresLayout = [[UICollectionViewFlowLayout alloc] init];
+        
+        pinteresLayout.itemSize = CGSizeMake(SCREEN_WIDTH, 333 * SCALE_6S_HEIGHT);
+        
+        pinteresLayout.minimumLineSpacing = 0;
+        
+        pinteresLayout.minimumInteritemSpacing = 0;
+        
+        _pinterestCollectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:pinteresLayout];
+        
+        _pinterestCollectionView.dataSource = self;
+        
+        _pinterestCollectionView.delegate = self;
+        
+        [_pinterestCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"pinterestCollectionViewCell"];
+    }
+    
+    return _pinterestCollectionView;
+}
+
+#pragma mark - view life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    [self.view addSubview:self.pinterestCollectionView];
 }
+
+#pragma mark - pinterestCollectionView datasource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 50;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.pinterestCollectionView dequeueReusableCellWithReuseIdentifier:@"pinterestCollectionViewCell" forIndexPath:indexPath];
+    
+    cell.contentView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255) / 255.0
+                                                       green:arc4random_uniform(255) / 255.0
+                                                        blue:arc4random_uniform(255) / 255.0
+                                                       alpha:1];
+    
+    return cell;
+}
+
+#pragma mark - pinterestCollectionView delegate
+
 
 
 
